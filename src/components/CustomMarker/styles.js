89bf6f2408687f34eeb13f478c5c9daf7929cc7c
Bruno De Masi <bruno.demasi@hotmail.com>
2019-05-16 04:45:00 -0300

    Fixing Marker Size

--- src/components/CustomMarker/styles.js
+++ src/components/CustomMarker/styles.js
@@ -2,11 +2,6 @@ import { StyleSheet, Dimensions } from 'react-native';
 import { colors, metrics } from '~/styles';
 
 const styles = StyleSheet.create({
-  marker: {
-    position: 'absolute',
-    backgroundColor: colors.white,
-  },
-
   callout: {
     backgroundColor: colors.secondary,
     padding: 10,
